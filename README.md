2ae01fad610b6053dbdec993ab8e0ffdae27b141
xujiang <xujiang@boohee.com>
2025-01-03 17:54:04 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rust Practice
+
+## 目录
+
+- [collection](./src/collection) 集合相关
+
+
+
+
+主要参考了 
+- [Rust 语言圣经](https://course.rs/about-book.html)
+- [Rust 程序设计语言](https://doc.rust-lang.org/book/title-page.html)
--- src/collection/hashmap.rs
+++ src/collection/hashmap.rs
@@ -1,1 +1,47 @@
+use std::collections::HashMap;
+
 // 这里可以放置 hashmap 模块的实现代码
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_hashmap() {
+        let mut h = HashMap::new();
+        h.insert("key", "value");
+        println!("{:?}", h);
+    }
+
+    #[test]
+    fn test_get_hashmap_item() {
+        let mut h = HashMap::new();
+        h.insert("key", "value");
+        let value = h.get("key");
+        println!("{:?}", value);
+    }
+
+    #[test]
+    fn test_borrow_hashmap() {
+        use std::collections::HashMap;
+
+        let name = String::from("Sunface");
+        let age = 18;
+
+        let mut handsome_boys = HashMap::new();
+        handsome_boys.insert(&name, age);
+
+        println!("因为过于无耻，{:?}已经被除名", handsome_boys);
+        println!("还有，他的真实年龄远远不止{}岁", age);
+        std::mem::drop(name);
+    }
+
+    #[test]
+    fn test_iter_hashmap() {
+        let mut h = HashMap::new();
+        h.insert("key", "value");
+        h.insert("key2", "value2");
+        for (key, value) in &h {
+            println!("{}: {}", key, value);
+        }
+    }
+}
